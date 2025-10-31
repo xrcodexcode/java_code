93de5ec0df92d80a2efde6cfc74dd0f304f273fc
Piyush Pal <136997670+xrcodexcode@users.noreply.github.com>
2025-10-31 23:58:39 +0530

    Create arraylist1.java

---
+++ java-ArrayList/arraylist1.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class arraylist 
+{
+    public static void main (String[] args) 
+  {
+        
+      	// Creating an ArrayList
+      	ArrayList<Integer> a = new ArrayList<Integer>();
+      	
+      	// Adding Element in ArrayList
+      	a.add(1);
+      	a.add(2);
+      	a.add(3);
+      
+      	// Printing ArrayList
+      	System.out.println(a);
+    }
+}
