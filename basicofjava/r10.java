634f78680638c83586604dec998da20d4eb0a726
Piyush Pal <xrpiyushh@gmail.com>
2025-11-02 15:59:17 +0530

    display

--- basicofjava/r10.java
+++ basicofjava/r10.java
@@ -7,4 +7,15 @@ public class r10
     System.out.println("Enter the number:");
     int n = in.nextInt();
    }
+   void display()
+   {
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+   }
 }
