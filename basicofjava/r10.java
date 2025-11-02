1e8b7c2353617cf86dddb73082a594ec8e43fefe
Piyush Pal <xrpiyushh@gmail.com>
2025-11-02 15:51:17 +0530

    input

--- basicofjava/r10.java
+++ basicofjava/r10.java
@@ -1,3 +1,10 @@
-public class r10 {
-    
+import java.util.*;
+public class r10 
+{
+   void input()
+   {
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the number:");
+    int n = in.nextInt();
+   }
 }
